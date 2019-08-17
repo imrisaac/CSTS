f43bf0d82f2fd5b5bca35c89fcddea69a8e12997
imrisaac@gmaik.com <ireed@altavian.com>
2019-08-17 18:59:15 -0400

    fix resolutions.h

--- src/resolutions.h
+++ src/resolutions.h
@@ -1,13 +1,18 @@
 #ifndef resolutions_h
 #define resolutions_h
 
+// AR1820 crop levels
 struct Zoom{
-    cv::Rect wide  = cv::Rect(0, 600, 960, 720);
+    cv::Rect wide  = cv::Rect(60, 600, 960, 720);
     cv::Rect tele0 = cv::Rect(0, 600, 1080, 810);
     cv::Rect tele1 = cv::Rect(0, 600, 1080, 810);
     cv::Rect tele2 = cv::Rect(0, 600, 1080, 810);
 };
 
+/*
+    Boson640
+    Sensor rotated 90 deg clockwise, looking into the lense 
+*/
 struct Boson640_90{
     int width = 512;
     int height = 640;
